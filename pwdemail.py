8bb6988836ec46645e4d17d260144658aa8cc0f3
mjjm19 <105559009+mjjm19@users.noreply.github.com>
2022-10-19 15:35:54 +0200

    Add files via upload

---
+++ pwdemail.py
@@ -0,0 +1,2 @@
+pwd='Joshan1998'
+#print('Password is:',pwd)
---
+++ replace.py
@@ -0,0 +1,6 @@
+def wr():
+    str='Hello, how is Mercy? I is fine. I is good. I is studying'
+    wordrepl=input('What is the word you want to replace?: ' )
+    replacement=input('What is the replacement?: ' )
+    print(str.replace(wordrepl,replacement))
+wr() 
